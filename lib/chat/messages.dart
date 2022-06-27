e8a69a5f96575548ef58ff0b81694e96dc8e1c89
nishi.patel <nishi.patel@zealousys.local>
2022-06-27 18:58:47 +0530

    chat

--- lib/chat/messages.dart
+++ lib/chat/messages.dart
@@ -9,35 +9,32 @@ class Messages extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return FutureBuilder<User>(
-      future: FirebaseAuth.instance.currentUser!,
+      future: Future.value(FirebaseAuth.instance.currentUser),
       builder: (ctx, futureSnapshot) {
         if (futureSnapshot.connectionState == ConnectionState.waiting) {
           return Center(child: CircularProgressIndicator());
         }
-        return
-          StreamBuilder<QuerySnapshot>(
-              builder: (ctx, chatSnapshot) {
-                if (chatSnapshot.connectionState == ConnectionState.waiting) {
-                  return Center(child: CircularProgressIndicator());
-                }
-                final chatDoc = chatSnapshot.data?.docs;
+        return StreamBuilder<QuerySnapshot>(
+          builder: (ctx, chatSnapshot) {
+            if (chatSnapshot.connectionState == ConnectionState.waiting) {
+              return Center(child: CircularProgressIndicator());
+            }
+            final chatDoc = chatSnapshot.data?.docs;
 
-
-                return ListView.builder(
-                  reverse: true,
-                  itemBuilder: (ctx, index) =>
-                      MessageBubble(
-                          chatDoc![index]['text'],
-                          chatDoc![index]['userId'] ==
-                              futureSnapshot.data?.uid),
-                  itemCount: chatDoc?.length,
-                );
-              });
+            return ListView.builder(
+              reverse: true,
+              itemBuilder: (ctx, index) => MessageBubble(
+                  chatDoc![index]['text'],
+                  chatDoc![index]['userId'] == futureSnapshot.data?.uid),
+              itemCount: chatDoc?.length,
+            );
+          },
+          stream: FirebaseFirestore.instance
+              .collection('chat')
+              .orderBy('createdAt', descending: true)
+              .snapshots(),
+        );
       },
-      stream: FirebaseFirestore.instance
-          .collection('chat')
-          .orderBy('createdAt', descending: true)
-          .snapshots(),
     );
   }
 }
